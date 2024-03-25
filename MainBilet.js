37a81b92b6b3e7ce2f9a49ba593dc874525cc7a0
DrFenomen <163646114+DrFenomen@users.noreply.github.com>
2024-03-25 07:41:37 +0300

    Update MainBilet.js

--- MainBilet.js
+++ MainBilet.js
@@ -1,13 +1,58 @@
 var bilet101=document.querySelector('.Info');
 var bilet102=document.querySelector('.Info1-2');
 var select=document.querySelector('.Nav_Selection');
-var test101=document.querySelector('.test101');
-var test102=document.querySelector('.test102');
 var bilet201=document.querySelector('.Info2-1');
 var bilet202=document.querySelector('.Info2-2');
 var bilet301=document.querySelector('.Info3-1');
+var bilet302=document.querySelector('.Info3-2');
+var bilet401=document.querySelector('.Info4-1');
+var bilet402=document.querySelector('.Info4-2');
+var bilet501=document.querySelector('.Info5-1');
+var bilet502=document.querySelector('.Info5-2');
+var bilet601=document.querySelector('.Info6-1');
+var bilet602=document.querySelector('.Info6-2');
+var bilet701=document.querySelector('.Info7-1');
+var bilet702=document.querySelector('.Info7-2');
+var bilet801=document.querySelector('.Info8-1');
+var bilet802=document.querySelector('.Info8-2');
+var bilet901=document.querySelector('.Info9-1');
+var bilet902=document.querySelector('.Info9-2');
+var bilet1001=document.querySelector('.Info10-1');
+var bilet1002=document.querySelector('.Info10-2');
+var bilet1101=document.querySelector('.Info11-1');
+var bilet1102=document.querySelector('.Info11-2');
+var bilet1201=document.querySelector('.Info12-1');
+var bilet1202=document.querySelector('.Info12-2');
+var bilet1301=document.querySelector('.Info13-1');
+var bilet1302=document.querySelector('.Info13-2');
+var bilet1401=document.querySelector('.Info14-1');
+var bilet1402=document.querySelector('.Info14-2');
+var bilet1501=document.querySelector('.Info15-1');
+var bilet1502=document.querySelector('.Info15-2');
+var bilet1601=document.querySelector('.Info16-1');
+var bilet1602=document.querySelector('.Info16-2');
+var bilet1701=document.querySelector('.Info17-1');
+var bilet1702=document.querySelector('.Info17-2');
+var bilet1801=document.querySelector('.Info18-1');
+var bilet1802=document.querySelector('.Info18-2');
+var bilet1901=document.querySelector('.Info19-1');
+var bilet1902=document.querySelector('.Info19-2');
+var bilet2001=document.querySelector('.Info20-1');
+var bilet2002=document.querySelector('.Info20-2');
+var bilet2101=document.querySelector('.Info21-1');
+var bilet2102=document.querySelector('.Info21-2');
+var bilet2201=document.querySelector('.Info22-1');
+var bilet2202=document.querySelector('.Info22-2');
+var bilet2301=document.querySelector('.Info23-1');
+var bilet2302=document.querySelector('.Info23-2');
+var bilet2401=document.querySelector('.Info24-1');
+var bilet2402=document.querySelector('.Info24-2');
+var bilet2501=document.querySelector('.Info25-1');
+var bilet2502=document.querySelector('.Info25-2');
+
 select.onchange = (e) => {
  var item = select.value;
+ console.log(item);
   if(item =='1.1')
   {
    bilet101.style.display = "inline";
@@ -43,5 +88,349 @@ select.onchange = (e) => {
  }
  else {
   bilet301.style.display = "none";
+ }
+ if(item =='3.2')
+ {
+  bilet302.style.display = "inline";
+ }
+ else {
+  bilet302.style.display = "none";
+ }
+ if(item =='4.1')
+ {
+  bilet401.style.display = "inline";
+ }
+ else {
+  bilet401.style.display = "none";
+ }
+ if(item =='4.2')
+ {
+  bilet402.style.display = "inline";
+ }
+ else
+ {
+  bilet402.style.display = "none";
+ }
+ if(item =='5.1')
+ {
+  bilet501.style.display = "inline";
+ }
+ else
+ {
+  bilet501.style.display = "none";
+ }
+ if(item =='5.2')
+ {
+  bilet502.style.display = "inline";
+ }
+ else
+ {
+  bilet502.style.display = "none";
+ }
+ if(item =='6.1')
+ {
+  bilet601.style.display = "inline";
+ }
+ else {
+  bilet601.style.display = "none";
+ }
+
+ if(item =='6.2')
+ {
+  bilet602.style.display = "inline";
+ }
+ else {
+  bilet602.style.display = "none";
+ }
+ if(item =='7.1')
+ {
+  bilet701.style.display = "inline";
+ }
+ else {
+  bilet701.style.display = "none";
+ }
+ if(item =='7.2')
+ {
+  bilet702.style.display = "inline";
+ }
+ else {
+  bilet702.style.display = "none";
+ }
+ if(item =='8.1')
+ {
+  bilet801.style.display = "inline";
+ }
+ else {
+  bilet801.style.display = "none";
+ }
+ if(item =='8.2')
+ {
+  bilet802.style.display = "inline";
+ }
+ else {
+  bilet802.style.display = "none";
+ }
+ if(item =='9.1')
+ {
+  bilet901.style.display = "inline";
+ }
+ else {
+  bilet901.style.display = "none";
+ }
+ if(item =='9.2')
+ {
+  bilet902.style.display = "inline";
+ }
+ else
+ {
+  bilet902.style.display = "none";
+ }
+ if(item =='10.1')
+ {
+  bilet1001.style.display = "inline";
+ }
+ else
+ {
+  bilet1001.style.display = "none";
+ }
+ if(item =='10.2')
+ {
+  bilet1002.style.display = "inline";
+ }
+ else
+ {
+  bilet1002.style.display = "none";
+ }
+ if(item =='11.1')
+ {
+  bilet1101.style.display = "inline";
+ }
+ else
+ {
+  bilet1101.style.display = "none";
+ }
+ if(item =='11.2')
+ {
+  bilet1102.style.display = "inline";
+ }
+ else
+ {
+  bilet1102.style.display = "none";
+ }
+ if(item =='12.1')
+ {
+  bilet1201.style.display = "inline";
+ }
+ else
+ {
+  bilet1201.style.display = "none";
+ }
+ if(item =='12.2')
+ {
+  bilet1202.style.display = "inline";
+ }
+ else
+ {
+  bilet1202.style.display = "none";
+ }
+ if(item =='13.1')
+ {
+  bilet1301.style.display = "inline";
+ }
+ else
+ {
+  bilet1301.style.display = "none";
+ }
+ if(item =='13.2')
+ {
+  bilet1302.style.display = "inline";
+ }
+ else
+ {
+  bilet1302.style.display = "none";
+ }
+ if(item =='14.1')
+ {
+  bilet1401.style.display = "inline";
+ }
+ else
+ {
+  bilet1401.style.display = "none";
+ }
+ if(item =='14.2')
+ {
+  bilet1402.style.display = "inline";
+ }
+ else
+ {
+  bilet1402.style.display = "none";
+ }
+ if(item =='15.1')
+ {
+  bilet1501.style.display = "inline";
+ }
+ else
+ {
+  bilet1501.style.display = "none";
+ }
+ if(item =='15.2')
+ {
+  bilet1502.style.display = "inline";
+ }
+ else
+ {
+  bilet1502.style.display = "none";
+ }
+ if(item =='16.1')
+ {
+  bilet1601.style.display = "inline";
+ }
+ else
+ {
+  bilet1601.style.display = "none";
+ }
+ if(item =='16.2')
+ {
+  bilet1602.style.display = "inline";
+ }
+ else
+ {
+  bilet1602.style.display = "none";
+ }
+ if(item =='17.1')
+ {
+  bilet1701.style.display = "inline";
+ }
+ else
+ {
+  bilet1701.style.display = "none";
+ }
+ if(item =='17.2')
+ {
+  console.log('1');
+  bilet1702.style.display = "inline";
+ }
+ else
+ {
+  bilet1702.style.display = "none";
+ }
+ if(item =='18.1')
+ {
+  bilet1801.style.display = "inline";
+ }
+ else
+ {
+  bilet1801.style.display = "none";
+ }
+ if(item =='18.2')
+ {
+  bilet1802.style.display = "inline";
+ }
+ else
+ {
+  bilet1802.style.display = "none";
+ }
+ if(item =='19.1')
+ {
+  bilet1901.style.display = "inline";
+ }
+ else
+ {
+  bilet1901.style.display = "none";
+ }
+ if(item =='19.2')
+ {
+  bilet1902.style.display = "inline";
+ }
+ else {
+  bilet1902.style.display = "none";
+ }
+ if(item =='20.2')
+ {
+  bilet2002.style.display = "inline";
+ }
+ else
+ {
+  bilet2002.style.display = "none";
+ }
+ if(item =='21.1')
+ {
+  bilet2101.style.display = "inline";
+ }
+ else
+ {
+  bilet2101.style.display = "none";
+ }
+ if(item =='21.2')
+ {
+  bilet2102.style.display = "inline";
+ }
+ else
+ {
+  bilet2102.style.display = "none";
+ }
+ if(item =='22.1')
+ {
+  bilet2201.style.display = "inline";
+ }
+ else
+ {
+  bilet2201.style.display = "none";
+ }
+ if(item =='22.2')
+ {
+  bilet2202.style.display = "inline";
+ }
+ else
+ {
+  bilet2202.style.display = "none";
+ }
+ if(item =='23.1')
+ {
+  bilet2301.style.display = "inline";
+ }
+ else
+ {
+  bilet2301.style.display = "none";
+ }
+ if(item =='23.2')
+ {
+  bilet2302.style.display = "inline";
+ }
+ else
+ {
+  bilet2302.style.display = "none";
+ }
+ if(item =='24.1')
+ {
+  bilet2401.style.display = "inline";
+ }
+ else
+ {
+  bilet2401.style.display = "none";
+ }
+ if(item =='24.2')
+ {
+  bilet2402.style.display = "inline";
+ }
+ else
+ {
+  bilet1402.style.display = "none";
+ }
+ if(item =='25.1')
+ {
+  bilet2501.style.display = "inline";
+ }
+ else
+ {
+  bilet2501.style.display = "none";
+ }
+ if(item =='25.2')
+ {
+  bilet2502.style.display = "inline";
+ }
+ else
+ {
+  bilet2502.style.display = "none";
  }
 }
